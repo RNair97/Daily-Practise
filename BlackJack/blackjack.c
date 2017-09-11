c1bcc8e080f448b7359a6238b99d2f24f8fd98f9
AvZon <rohan_nair97@hotmail.com>
2017-09-11 00:46:01 -0400

    Full Loop Functionality

--- BlackJack/blackjack.c
+++ BlackJack/blackjack.c
@@ -20,8 +20,12 @@ int main() {
     totalPlayer1 = 0;
     totalPlayer2 = 0;
 
-    bool player_one_turn = false;
+    bool player_one_turn = true;
     bool player_two_turn = false;
+
+    bool player_one_hold = false;
+    bool player_two_hold = false;
+
 
 
     printf("\nPlayer 1, enter your name : ");
@@ -35,76 +39,115 @@ int main() {
 
     do {
 
-    do {
+        if (player_one_turn == true) {
 
-        printf("\n1. Hit\n");
-        printf("2. Stay\n");
+            printf("\n1. Hit\n");
+            printf("2. Stay\n");
 
-        printf("%s, please select an option : ", nameOne);
-        scanf("%d", &optionOne);
+            printf("%s, please select an option : ", nameOne);
+            scanf("%d", &optionOne);
 
-        if (optionOne == 1) {
+            if (optionOne == 1) {
 
-            rndNum1 = (rand() % 13) + 1;
-            totalPlayer1 = totalPlayer1 + rndNum1;
+                rndNum1 = (rand() % 13) + 1;
+                totalPlayer1 = totalPlayer1 + rndNum1;
 
-            printf("%s hits! Total is : %d\n", nameOne, totalPlayer1);
+                printf("%s hits! Total is : %d\n", nameOne, totalPlayer1);
 
-        } else {
 
-            totalPlayer1 = totalPlayer1;
+            } else {
 
-            printf("%s stays! Total is %d\n", nameOne, totalPlayer1);
+                printf("%s stays! Total is %d\n", nameOne, totalPlayer1);
 
-            player_one_turn = false;
+                player_one_hold = true;
+
+            }
+
+            if (player_two_hold == true) {
+
+
+                if (player_one_hold == true) {
+
+                    player_one_turn = false;
+                    player_two_turn = false;
+
+                } else {
+
+                    player_one_turn = true;
+                    player_two_turn = false;
+
+                }
+
+            } else {
+
+                player_one_turn = false;
+                player_two_turn = true;
+
+            }
+
 
         }
 
 
-    } while(player_one_turn == true);
+        if (player_two_turn == true) {
 
-    do {
+            printf("\n1. Hit\n");
+            printf("2. Stay\n");
 
-        printf("\n1. Hit\n");
-        printf("2. Stay\n");
+            printf("%s, please select an option : ", nameTwo);
+            scanf("%d", &optionTwo);
 
-        printf("%s, please select an option : ", nameTwo);
-        scanf("%d", &optionTwo);
+            if (optionTwo == 1) {
 
-        if (optionTwo == 1) {
+                rndNum2 = (rand() % 13) + 1;
+                totalPlayer2 = totalPlayer2 + rndNum2;
 
-            rndNum2 = (rand() % 13) + 1;
-            totalPlayer2 = totalPlayer2 + rndNum2;
+                printf("%s hits! Total is : %d\n", nameTwo, totalPlayer2);
 
-            printf("%s hits! Total is : %d\n", nameTwo, totalPlayer2);
+            } else {
 
+                printf("%s stays! Total is %d\n", nameTwo, totalPlayer2);
 
-        } else {
+                player_two_hold = true;
 
-            totalPlayer2 = totalPlayer2;
+            }
 
-            printf("%s stays! Total is %d\n", nameTwo, totalPlayer2);
+            if (player_one_hold == true) {
 
-            player_two_turn = false;
+                if (player_two_hold == true) {
+
+                    player_one_turn = false;
+                    player_two_turn = false;
+
+                } else {
+
+                    player_one_turn = false;
+                    player_two_turn = true;
+
+                }
+
+            } else {
+
+                player_one_turn = true;
+                player_two_turn = false;
+
+            }
 
         }
 
 
-    } while(player_two_turn == true);
+    } while(player_one_turn == true || player_two_turn == true);
 
-} while(player_one_turn == false && player_two_turn == false);
 
     if (totalPlayer1 > totalPlayer2) {
 
-        printf("%s wins!\n", nameOne);
+        printf("\n%s wins!\n", nameOne);
 
     } else {
 
-        printf("%s wins!\n", nameTwo);
-        
+        printf("\n%s wins!\n", nameTwo);
+
     }
-
-
 
     return 0;
 }
