023fb3aae72a55d4cb0939ca8cc89083047f2dc7
AvZon <rohan_nair97@hotmail.com>
2017-09-12 19:50:06 -0400

    Data Types notes 1

---
+++ Data Types/data_types.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    /* Pieces of data */
+
+    /* short (16 bit single precision value) */
+    /* int (32 bit single precision value) */
+    /* long (64 bit single precision value) */
+    /* float (32 bit single precision floating point value) */
+    /* double (64 bit double precision floating point value */
+    /* char (1 bit value) */
+    /* bool (1 bit value) */
+
+    short shortValue = 0;
+    int integerValue = 1;
+    long longValue = 2;
+
+    printf("Short: %d\n", shortValue);
+    printf("Int: %d\n", integerValue);
+    printf("Long: %ld\n", longValue);
+
+    return 0;
+}
