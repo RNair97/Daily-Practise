2a94176d3b6431502b2e6a6016ac6011e4d0edb3
AvZon <rohan_nair97@hotmail.com>
2017-09-12 20:39:39 -0400

    More Functions Practise

--- Data Types/functions.c
+++ Data Types/functions.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 void hello_world();
+void print_int(int value); //same as exact funtion //initialization
 
 int main() {
 
     hello_world();
+    print_int(655);
 
     return 0;
 }
@@ -12,5 +14,11 @@ int main() {
 void hello_world() {
 
     printf("Hello World!\n");
+
+}
+
+void print_int(int value) { //declaration
+
+    printf("%d\n", value);
 
 }
